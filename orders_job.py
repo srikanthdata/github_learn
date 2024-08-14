c725ccd058c2c632e5c39f6839fb4d94198c6b69
srikanthdata <srikanthdata12@gmail.com>
2024-08-14 18:06:11 +0530

    second commit

--- orders_job.py
+++ orders_job.py
@@ -1,5 +1,6 @@
 # Set the task dependencies
 get_execution_date_task >> submit_pyspark_job
+get_execution_date_task == submit_pyspark_job
 
 
 # arguments=['--start_date=2024-01-02', '--end_date=2024-01-03']
